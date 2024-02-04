6c19b6285ade1385431d899170a5a7eed71fa73a
Dane Grant <danecando@gmail.com>
2024-02-04 09:32:44 -0500

    add event listener and logs

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,8 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "modal" ]
+  static targets = [ "modal", "frame" ]
   connect() {
+    this.frameTarget.addEventListener('turbo:before-morph-attribute', (event) => {
+      if (event.detail.attributeName === 'src') {
+        console.log('turbo:before-morph-attribute src', event.detail.newValue)
+      }
+    });
+
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (mutation.attributeName === 'src') {
+          const src = mutation.target.src;
+          console.log('frame mutation observer new src: ', src)
+        }
+      });
+    });
+    observer.observe(this.frameTarget, { attributes: true });
+
     this.element.addEventListener('turbo:frame-load', () => {
       this.open()
     })
